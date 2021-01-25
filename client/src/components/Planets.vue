3a7ca881cfb9e318687c7138720024cc9c0d1e99
evelyn-sz <evelynszarek@gmail.com>
2021-01-25 14:14:15 +0000

    Added images for Planet List

--- client/src/components/Planets.vue
+++ client/src/components/Planets.vue
@@ -3,7 +3,18 @@
       <h2>List of planets</h2>
     <ul>
       <li v-for="planet in planets">
+        <span>
+        <img class="icon" v-if="planet.englishName == 'Mercury'" src="@/assets/Planets/Mercury.png" />
+        <img class="icon" v-if="planet.englishName == 'Venus'" src="@/assets/Planets/Venus.png" />
+        <img class="icon" v-if="planet.englishName == 'Earth'" src="@/assets/Planets/Earth.png" />
+        <img class="icon" v-if="planet.englishName == 'Mars'" src="@/assets/Planets/Mars.png" />
+        <img class="icon" v-if="planet.englishName == 'Saturn'" src="@/assets/Planets/Saturn.png" />
+        <img class="icon" v-if="planet.englishName == 'Jupiter'" src="@/assets/Planets/Jupiter.png" />
+        <img class="icon" v-if="planet.englishName == 'Uranus'" src="@/assets/Planets/Uranus.jpg" />
+        <img class="icon" v-if="planet.englishName == 'Neptune'" src="@/assets/Planets/Neptune.png" />
+        <img class="icon" v-if="planet.englishName == 'Pluto'" src="@/assets/Planets/Pluto.jpg" />
         <p v-if="planet.englishName">{{planet.englishName}}</p>
+        </span>
       </li>
     </ul>
   </div>
@@ -29,8 +40,8 @@ export default {
     },
     sortedByDistanceFromSun: function () {
       function compare(a, b)  {
-        if (a.sideralOrbit < b.sideralOrbit) return -1;
-        if (a.sideralOrbit > b.sideralOrbit) return 1;
+        if (a.semimajorAxis < b.semimajorAxis) return -1;
+        if (a.semimajorAxis > b.semimajorAxis) return 1;
         return 0;
       }
       return this.planets.sort(compare)
@@ -40,5 +51,19 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.icon {
+  width: 30px
+}
+
+span {
+  display: flex;
+  align-items: center;
+}
+.icon {
+width: 30px;
+height: 30px;
+padding: 3px
+}
+
 
 </style>

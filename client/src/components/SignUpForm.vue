083b9c780a371e9501156de6ae1ad46f5eefbdaa
milacerveira <milascerveira@gmail.com>
2021-01-25 17:22:28 +0000

    amending form component

--- client/src/components/SignUpForm.vue
+++ client/src/components/SignUpForm.vue
@@ -2,15 +2,15 @@
     <div>
     <form id="signup-form">
        <div class="form-item">
-        <label class="label">First Name</label>
+        <label class="label"></label>
         <input type="text" placeholder="First Name" class="input" first="first" v-model=first>
       </div>
       <div class="form-item">
-        <label class="label">Last Name</label>
+        <label class="label"></label>
         <input type="text" placeholder="Last Name" class="input" last="last" v-model=last>
       </div>
       <div class="form-item">
-        <label class="label">Email</label>
+        <label class="label"></label>
         <input type="email" placeholder="Email" class="input" name="email" v-model="email">
       </div>
       <button type="submit" class="button" @click.prevent>Submit</button>
@@ -30,5 +30,7 @@ data(){
 }
 </script>
 <style scoped>
-
+#signup-form{
+  margin-left: 40px;
+}
 </style>

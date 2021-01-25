fb80b0967a920101f84fb05314167661f1042053
milacerveira <milascerveira@gmail.com>
2021-01-25 19:33:39 +0000

    added linkedin icon

--- client/src/App.vue
+++ client/src/App.vue
@@ -74,6 +74,7 @@
 <a href="#" class="fa fa-facebook"></a>
 <a href="#" class="fa fa-twitter"></a>
 <a href='#' class="fa fa-instagram"></a>
+<a href='#' class='fa fa-linkedin'></a>
 </div>
 
 </div>
